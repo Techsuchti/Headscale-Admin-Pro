55634fbf7fd0bda80a13db56cc2604d2e62255e5
Techsuchti <techsuchti@ik.me>
2026-01-21 01:22:08 +0100

    Translate comments in index.js from Chinese to German

--- static/index.js
+++ static/index.js
@@ -1,13 +1,13 @@
 /**
- * 初始化主题入口模块
+ * Initialisierung des Theme-Einstiegsmoduls
  */
 
 layui.extend({
-  setter: 'config' // 将 config.js 扩展到 layui 模块
+  setter: 'config' // config.js als layui-Modul erweitern
 }).define(['setter'], function(exports){
   var setter = layui.setter;
 
-  // 将核心库扩展到 layui 模块
+  // Kernbibliothek als layui-Module erweitern
   layui.each({
     admin: 'admin',
     view: 'view',
@@ -18,23 +18,23 @@ layui.extend({
     layui.extend(libs);
   });
 
-  // 指定业务模块基础目录
+  // Basisverzeichnis der Business-Module festlegen
   layui.config({
     base: setter.paths.modules
   });
 
-  // 将业务模块中的特殊模块扩展到 layui 模块
+  // Spezielle Module aus den Business-Modulen als layui-Module erweitern
   layui.each(setter.extend, function(key, value){
     var mods = {};
     mods[key] = '{/}' + layui.cache.base + value;
     layui.extend(mods);
   });
 
-  // 加载主题核心库入口模块
+  // Einstiegsmodul der Theme-Kernbibliothek laden
   layui.use('adminIndex', function(){
-    layui.use('common'); // 加载公共业务模块，如不需要可剔除
+    layui.use('common'); // Öffentliches Business-Modul laden; kann entfernt werden, wenn nicht benötigt
 
-    // 输出模块 / 模块加载完毕标志
+    // Modul exportieren / Marker: Module wurden geladen
     exports('index', layui.admin);
   });
 });
